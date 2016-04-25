70d58fb1aa7894f04a8ce7e592f6bc2d953e8e53
mikjongo <mikepsw@yahoo.com>
2016-04-25 03:26:46 +0200

    local at 03-26

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ stamp:<? echo $ctools->getTimeStamp()?>
     | <a href="phpinfo.php">phpinfo</a>
 </p>
 
-<br>we added this... and this remote at 03-21
+<br>we added this... and this remote at 03-21, local 03-24
 
 <!-- form -->
 <form action = "<?php $_PHP_SELF ?>" method = "POST">
